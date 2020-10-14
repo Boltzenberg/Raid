17de7545b822047092c220bd9fe3c0074f8d4c1e
Jon Rosenberg <jon_rosenberg@hotmail.com>
2020-10-13 23:06:42 -0700

    Add Ash's team and Deadwood Jedi's team and refactor champion creation.

--- ClanBossTurns/Program.cs
+++ ClanBossTurns/Program.cs
@@ -14,132 +14,22 @@ namespace ClanBossTurns
 {
     class Program
     {
-        static Tuple<Champion, List<SkillPolicy>> CreateManeater()
-        {
-            List<Skill> skills = new List<Skill>();
-            skills.Add(new Skill("Pummel", Constants.SkillId.A1, 0, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Syphon", Constants.SkillId.A2, 3, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill(
-                "Ancient Blood", 
-                Constants.SkillId.A3, 
-                5, 
-                new TurnAction(
-                    0, 
-                    Constants.Target.None, 
-                    null, 
-                    new List<BuffToApply>() { 
-                        new BuffToApply(Constants.Buff.BlockDebuffs, 2, Constants.Target.FullTeam), 
-                        new BuffToApply(Constants.Buff.Unkillable, 2, Constants.Target.FullTeam) 
-                    }, 
-                null)));
-
-            List<SkillPolicy> policies = new List<SkillPolicy>()
-            {
-                new SkillPolicy(Constants.SkillId.A3, 0),
-                new SkillPolicy(Constants.SkillId.A2, 0),
-                new SkillPolicy(Constants.SkillId.A1, 0),
-            };
-
-            return new Tuple<Champion, List<SkillPolicy>>(new Champion("Maneater", 98, 227, 20310, 1112, 2, skills), policies);
-        }
-
-        static Tuple<Champion, List<SkillPolicy>> CreatePainkeeper()
-        {
-            List<Skill> skills = new List<Skill>();
-            skills.Add(new Skill(
-                "Unflagging Advance",
-                Constants.SkillId.A1,
-                0,
-                new TurnAction(1, Constants.Target.OneEnemy, new List<EffectToApply>() { new EffectToApply(Constants.Effect.FillTurnMeterBy10Percent, Constants.Target.Self) }, null, null)));
-            skills.Add(new Skill("Spectacular Sweep", Constants.SkillId.A2, 4, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill(
-                "Combat Tactics",
-                Constants.SkillId.A3,
-                4,
-                new TurnAction(0, Constants.Target.None, new List<EffectToApply>() { new EffectToApply(Constants.Effect.ReduceCooldownBy1, Constants.Target.AllAllies) }, null, null)));
-
-            List<SkillPolicy> policies = new List<SkillPolicy>()
-            {
-                new SkillPolicy(Constants.SkillId.A3, 0),
-                new SkillPolicy(Constants.SkillId.A2, 3),
-                new SkillPolicy(Constants.SkillId.A1, 0),
-            };
-
-            return new Tuple<Champion, List<SkillPolicy>>(new Champion("Painkeeper", 102, 213, 19320, 771, 2, skills), policies);
-        }
-
-        static Tuple<Champion, List<SkillPolicy>> CreateFrozenBanshee()
-        {
-            List<Skill> skills = new List<Skill>();
-            skills.Add(new Skill("Death's Caress", Constants.SkillId.A1, 0, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Cruel Exultation", Constants.SkillId.A2, 3, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Frost Blight", Constants.SkillId.A3, 3, TurnAction.AttackOneEnemy()));
-
-            List<SkillPolicy> policies = new List<SkillPolicy>()
-            {
-                new SkillPolicy(Constants.SkillId.A3, 0),
-                new SkillPolicy(Constants.SkillId.A1, 0),
-            };
-
-            return new Tuple<Champion, List<SkillPolicy>>(new Champion("Frozen Banshee", 99, 169, 13440, 916, 0, skills), policies);
-        }
-
-        static Tuple<Champion, List<SkillPolicy>> CreateGravechillKiller()
-        {
-            List<Skill> skills = new List<Skill>();
-            skills.Add(new Skill("Freezing Toxin", Constants.SkillId.A1, 0, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Blood Chill", Constants.SkillId.A2, 3, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Icy Veins", Constants.SkillId.A3, 3, TurnAction.AttackOneEnemy()));
-
-            List<SkillPolicy> policies = new List<SkillPolicy>()
-            {
-                new SkillPolicy(Constants.SkillId.A3, 0),
-                new SkillPolicy(Constants.SkillId.A2, 0),
-                new SkillPolicy(Constants.SkillId.A1, 0),
-            };
-
-            return new Tuple<Champion, List<SkillPolicy>>(new Champion("Gravechill Killer", 96, 168, 12375, 815, 0, skills), policies);
-        }
-
-        static Tuple<Champion, List<SkillPolicy>> CreateBulwark()
-        {
-            List<Skill> skills = new List<Skill>();
-            skills.Add(new Skill("Hefty Flail", Constants.SkillId.A1, 0, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Meteoric Ignition", Constants.SkillId.A2, 3, TurnAction.AttackOneEnemy()));
-            skills.Add(new Skill("Punishing Defense", Constants.SkillId.P1, 0, TurnAction.AttackOneEnemy()));
-
-            List<SkillPolicy> policies = new List<SkillPolicy>()
-            {
-                new SkillPolicy(Constants.SkillId.A2, 0),
-                new SkillPolicy(Constants.SkillId.A1, 0),
-            };
-
-            return new Tuple<Champion, List<SkillPolicy>>(new Champion("Bulwark", 97, 106, 10125, 924, 0, skills), policies);
-        }
-
-        private delegate Tuple<Champion, List<SkillPolicy>> CreateChampion();
-
         static void Main(string[] args)
         {
-            List<CreateChampion> championCreators = new List<CreateChampion>()
-            {
-                CreateManeater,
-                CreatePainkeeper,
-                CreateFrozenBanshee,
-                CreateGravechillKiller,
-                CreateBulwark
-            };
+            ClanBoss.Level clanBossLevel = ClanBoss.Level.UltraNightmare;
+
+            List<Teams.CreateChampion> championCreators = Teams.DeadwoodJedi.ChampionCreators();
 
             List<Champion> champions = new List<Champion>();
             Dictionary<Champion, List<SkillPolicy>> skillPoliciesByChampion = new Dictionary<Champion, List<SkillPolicy>>();
-            foreach (CreateChampion cc in championCreators)
+            foreach (Teams.CreateChampion cc in championCreators)
             {
-                Tuple<Champion, List<SkillPolicy>> tuple = cc();
+                Tuple<Champion, List<SkillPolicy>> tuple = cc(clanBossLevel);
                 champions.Add(tuple.Item1);
                 skillPoliciesByChampion[tuple.Item1] = tuple.Item2;
             }
 
-            ClanBossBattle battle = new ClanBossBattle(ClanBoss.Level.Nightmare, champions, skillPoliciesByChampion);
+            ClanBossBattle battle = new ClanBossBattle(clanBossLevel, champions, skillPoliciesByChampion);
             List<ClanBossBattleResult> results = battle.Run();
             Console.WriteLine();
             Console.WriteLine("Run is over!");
---
+++ ClanBossTurns/Teams.cs
@@ -0,0 +1,262 @@
+﻿using RaidLib.DataModel;
+using System;
+using System.Collections.Generic;
+
+namespace ClanBossTurns
+{
+    public static class Teams
+    {
+        public delegate Tuple<Champion, List<SkillPolicy>> CreateChampion(ClanBoss.Level level);
+
+        public static class Gunga
+        {
+            static Tuple<Champion, List<SkillPolicy>> CreateManeater(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateManeater(227, 2), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreatePainkeeper(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 3),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreatePainkeeper(213, 2), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateFrozenBanshee(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateFrozenBanshee(169, 0), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateGravechillKiller(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateGravechillKiller(168, 0), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateBulwark(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateBulwark(106, 0), policies);
+            }
+
+            public static List<CreateChampion> ChampionCreators()
+            {
+                return new List<CreateChampion>()
+                {
+                    CreateManeater,
+                    CreatePainkeeper,
+                    CreateFrozenBanshee,
+                    CreateGravechillKiller,
+                    CreateBulwark
+                };
+            }
+        }
+
+        public static class Ash
+        {
+            static Tuple<Champion, List<SkillPolicy>> CreateManeater(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateManeater(98 + 143, 3), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreatePainkeeper(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 3),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreatePainkeeper(102 + 119, 1), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateFrozenBanshee(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateFrozenBanshee(99 + 78, 0), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateOccultBrawler(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateOccultBrawler(98 + 77, 0), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateAothar(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateAothar(92 + 24, 0), policies);
+            }
+
+            public static List<CreateChampion> ChampionCreators()
+            {
+                return new List<CreateChampion>()
+                {
+                    CreateManeater,
+                    CreatePainkeeper,
+                    CreateFrozenBanshee,
+                    CreateOccultBrawler,
+                    CreateAothar
+                };
+            }
+        }
+
+        public static class DeadwoodJedi
+        {
+            static Tuple<Champion, List<SkillPolicy>> CreateManeater(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>();
+
+                switch (level)
+                {
+                    case ClanBoss.Level.Brutal:
+                        break;
+                    case ClanBoss.Level.Nightmare:
+                        break;
+                    case ClanBoss.Level.UltraNightmare:
+                        policies.Add(new SkillPolicy(Constants.SkillId.A3, 0));
+                        policies.Add(new SkillPolicy(Constants.SkillId.A2, 0));
+                        policies.Add(new SkillPolicy(Constants.SkillId.A1, 0));
+                        break;
+                    default:
+                        throw new ArgumentException("Only works for Brutal, Nightmare, or UltraNightmare");
+                }
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateManeater(240, 3), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreatePainkeeper(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>();
+
+                switch (level)
+                {
+                    case ClanBoss.Level.Brutal:
+                        break;
+                    case ClanBoss.Level.Nightmare:
+                        break;
+                    case ClanBoss.Level.UltraNightmare:
+                        policies.Add(new SkillPolicy(Constants.SkillId.A3, 0));
+                        policies.Add(new SkillPolicy(Constants.SkillId.A2, 3));
+                        policies.Add(new SkillPolicy(Constants.SkillId.A1, 0));
+                        break;
+                    default:
+                        throw new ArgumentException("Only works for Brutal, Nightmare, or UltraNightmare");
+                }
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreatePainkeeper(221, 3), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateAothar(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                int uiSpeed = 114;
+                if (level == ClanBoss.Level.Nightmare)
+                {
+                    uiSpeed = 121;
+                }
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateAothar(uiSpeed, 1), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateRhazinScarhide(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A3, 0),
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateRhazinScarhide(177, 1), policies);
+            }
+
+            static Tuple<Champion, List<SkillPolicy>> CreateSeptimus(ClanBoss.Level level)
+            {
+                List<SkillPolicy> policies = new List<SkillPolicy>()
+                {
+                    new SkillPolicy(Constants.SkillId.A2, 0),
+                    new SkillPolicy(Constants.SkillId.A1, 0),
+                };
+
+                return new Tuple<Champion, List<SkillPolicy>>(Champion.CreateSeptimus(175, 2), policies);
+            }
+
+            public static List<CreateChampion> ChampionCreators()
+            {
+                return new List<CreateChampion>()
+                {
+                    CreateManeater,
+                    CreatePainkeeper,
+                    CreateRhazinScarhide,
+                    CreateSeptimus,
+                    CreateAothar
+                };
+            }
+        }
+
+        public static class BTrain
+        {
+
+        }
+    }
+}
